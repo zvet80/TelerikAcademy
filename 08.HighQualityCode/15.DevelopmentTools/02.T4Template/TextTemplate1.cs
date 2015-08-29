8bd1fdfe6975863fe2a6e94c3089f4e356371ce2
zvet80 <zvet80@hotmail.com>
2015-08-29 23:23:49 +0300

    ..

---
+++ 08.HighQualityCode/15.DevelopmentTools/02.T4Template/TextTemplate1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class MyClass
+{
+ 
+	public int Variable1 {get; set;}
+ 
+	public int Variable2 {get; set;}
+ 
+	public int Variable3 {get; set;}
+ 
+	public void Method1()
+	{
+		throw new NotImplementedException();
+	}
+ 
+	public void Method2()
+	{
+		throw new NotImplementedException();
+	}
+ 
+	public void Method3()
+	{
+		throw new NotImplementedException();
+	}
+
+
+
+}
